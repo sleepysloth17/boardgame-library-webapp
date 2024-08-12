b8f44c544369d458770c91d1fd37b9ec8b7a74cb
Your Name <you@example.com>
2024-08-13 01:29:53 +0200

    gonna start with the inputs I'll use for everything

---
+++ src/app/boardgames/filter-list/filter/input/checklist/checklist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checklist',
+  standalone: true,
+  imports: [],
+  templateUrl: './checklist.component.html',
+  styleUrl: './checklist.component.scss',
+})
+export class ChecklistComponent {}
---
+++ src/app/boardgames/filter-list/filter/input/select/select.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select',
+  standalone: true,
+  imports: [],
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.scss',
+})
+export class SelectComponent {}
